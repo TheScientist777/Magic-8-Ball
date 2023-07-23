8892abf953fd58290563e471ca905fcd3d1be2a0
The_Scientist777 <pmrojas195@gmail.com>
2023-07-23 18:09:48 -0500

    starting to look like a magic 8 ball

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-const ballContainer = document
+const ballContainer = document.querySelector('#ball-container')
+
+const answer = document.createElement('p')
+
+answer.classList.add('fade', 'text-center', 'fs-4', 'fw-bold', 'bg-white', 
+'text-primary', 'p-3', 'rounded', 'w-50', 'position-absolute', 'top-50', 
+'start-50','translate-middle')
+
+answer.innerText = 'Hello There, how are you doing?'
+
+ballContainer
